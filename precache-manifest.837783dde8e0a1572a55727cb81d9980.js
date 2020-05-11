8562757af77201393b50fe8cfb3ec76ba9e192f5
olegrichweb <60315178+olegrichweb@users.noreply.github.com>
2020-05-11 21:04:38 +0300

    Updates

---
+++ precache-manifest.837783dde8e0a1572a55727cb81d9980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78637f4ab855bbce3efb5c16513ef7d0",
+    "url": "/TimerApp/index.html"
+  },
+  {
+    "revision": "545c40f0c599784a0209",
+    "url": "/TimerApp/static/css/main.9599c6e8.chunk.css"
+  },
+  {
+    "revision": "9e51d00d83a5da1dfa9a",
+    "url": "/TimerApp/static/js/2.d6dad870.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TimerApp/static/js/2.d6dad870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545c40f0c599784a0209",
+    "url": "/TimerApp/static/js/main.0a6959e0.chunk.js"
+  },
+  {
+    "revision": "aae95bd735af8109c282",
+    "url": "/TimerApp/static/js/runtime-main.b764d78c.js"
+  }
+]);
